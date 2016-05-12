b217f251cd4141462d864e32434186559053760a
Lars Bilke <lars.bilke@ufz.de>
2016-05-12 09:05:23 +0200

    Run test in correct working directory.

--- test/conanfile.py
+++ test/conanfile.py
@@ -11,8 +11,12 @@ class CVODEReuseConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake . %s' % cmake.command_line)
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
+
     def test(self):
-        self.run(os.sep.join([".","bin", "mycvode"]))
+        self.run("cd bin && ./mycvode")
