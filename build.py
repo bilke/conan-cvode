e4f54bb549ee75e9878bffa95c85c41addcd33ad
Lars Bilke <lars.bilke@ufz.de>
2017-07-20 11:50:45 +0200

    Upgrade appveyor file.

--- build.py
+++ build.py
@@ -3,6 +3,4 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bilke")
     builder.add_common_builds(pure_c=False)
-    # builder.add({"arch": "x86_64", "build_type": "Release"}, {"CVODE:shared": False})
-    # builder.add({"arch": "x86_64", "build_type": "Debug"}, {"CVODE:shared": False})
     builder.run()
